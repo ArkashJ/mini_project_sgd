1dca0c4d3a3651093a3361b1c1fd5928c7406648
Arkash Jain <86805856+ArkashJ@users.noreply.github.com>
2023-10-21 09:56:11 -0400

    running experiments with changing epochs

--- run_sgd_experiments.py
+++ run_sgd_experiments.py
@@ -64,7 +64,7 @@ class run_experiments:
         batchsize,
         n,
         stepsize,
-        epochs: int = 5,
+        epochs,
         x=np.zeros(D + 1),
         alpha=0,
         avg_range: float = 0.5,
@@ -72,26 +72,14 @@ class run_experiments:
         params = run_sgd(grad_loss, epochs, x, stepsize, alpha, batchsize, n)
 
         x_iterate = params[-1]
-        print("x_iterate- \n", x_iterate)
 
         k = epochs * n // batchsize
         lower_range, upper_range = math.floor(k * avg_range), k + 1
         for i in range(lower_range, upper_range):
             x_iterate += params[i]
         x_iterate_average = x_iterate / (upper_range - lower_range)
-        print("x_iterate_average- \n", x_iterate_average)
 
         return x_iterate, x_iterate_average
-
-    def find_norm(
-        self,
-        x_star: np.ndarray,
-        x_iterate: np.ndarray,
-        x_iterate_average: np.ndarray,
-    ) -> float:
-        return np.linalg.norm(x_star - x_iterate), np.linalg.norm(
-            x_star - x_iterate_average
-        )
 
     # x_start = argmin (1/N)* sum(robust_loss(psi, beta, nu, Y, Z)) + (1/2N)*sum(beta**2)
     def estimate_x_star(self) -> Optional[np.ndarray]:
@@ -104,13 +92,26 @@ class run_experiments:
                 init_param,
                 args=(np.arange(self.N),),
             )
-            print("res_minimize- \n", res_minimize)
             print(f"res_minimize.x- \n {res_minimize.x}")
 
             return res_minimize.x, grad_sgd_loss
         except ValueError as e:
             print("Error in estimate_x_star")
             print(e)
+
+    def find_norm(
+        self,
+        x_star: np.ndarray,
+        x_iterate: np.ndarray,
+        x_iterate_average: np.ndarray,
+    ) -> float:
+        x_iterate = np.linalg.norm(x_star - x_iterate)
+        x_iterate_average = np.linalg.norm(x_star - x_iterate_average)
+        print(f"norm_x_star- \n {x_iterate}")
+        print(f"norm_x_tilda_k- \n {x_iterate_average}")
+        return np.linalg.norm(x_star - x_iterate), np.linalg.norm(
+            x_star - x_iterate_average
+        )
 
     def test_norms(self):
         x_star, grad_sgd_loss = self.estimate_x_star()
@@ -120,18 +121,56 @@ class run_experiments:
             self.B,
             self.N,
             self.ETA,
+            10,
         )
+
         norm_x_star, norm_x_tilda_k = self.find_norm(
             x_star, x_iterate, x_iterate_average
         )
-        print(f"norm_x_star- \n {norm_x_star}")
-        print(f"norm_x_tilda_k- \n {norm_x_tilda_k}")
+
+    def find_best_num_epochs(self):
+        epoch_increase = 5
+        epoch1 = [i for i in range(1, 100, epoch_increase)]
+        epoch2 = [i for i in range(100, 1000, epoch_increase * 10)]
+
+        x_star, grad_sgd_loss = self.estimate_x_star()
+        store_x_star = [0] * len(epoch1) + [0] * len(epoch2)
+        store_x_tilda_k = [0] * len(epoch1) + [0] * len(epoch2)
+
+        for idx, epoch in enumerate(epoch1):
+            x_iterate, x_iterate_average = self.estimate_x_tilda_k(
+                grad_sgd_loss,
+                self.B,
+                self.N,
+                self.ETA,
+                epoch,
+            )
+            norm_x_star, norm_x_tilda_k = self.find_norm(
+                x_star, x_iterate, x_iterate_average
+            )
+            store_x_star[idx] = norm_x_star
+            store_x_tilda_k[idx] = norm_x_tilda_k
+        epoch1_len = len(epoch1)
+        for idx, epoch in enumerate(epoch2):
+            x_iterate, x_iterate_average = self.estimate_x_tilda_k(
+                grad_sgd_loss,
+                self.B,
+                self.N,
+                self.ETA,
+                epoch,
+            )
+            norm_x_star, norm_x_tilda_k = self.find_norm(
+                x_star, x_iterate, x_iterate_average
+            )
+            store_x_star[idx + epoch1_len] = norm_x_star
+            store_x_tilda_k[idx + epoch1_len] = norm_x_tilda_k
+        print(f"store_x_star- \n {store_x_star}")
+        print(f"store_x_tilda_k- \n {store_x_tilda_k}")
 
 
 def main():
     experiments = run_experiments(N, D, NU, ETA, ETA_0, ALPHA, B)
     # experiments.init_param_test()
-    experiments.test_norms()
-
+    experiments.find_best_num_epochs()
 
 main()
